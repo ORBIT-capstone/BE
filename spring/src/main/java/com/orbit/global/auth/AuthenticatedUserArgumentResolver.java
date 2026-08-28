90cfe6793b920e0b8a9de3f08501a1109ecd42c2
hyunseoko <kohyun2002223@gmail.com>
2026-08-28 23:43:40 +0900

    ♻️ Refactor: Spring 인증 구조 개선

---
+++ spring/src/main/java/com/orbit/global/auth/AuthenticatedUserArgumentResolver.java
@@ -0,0 +1,34 @@
+package com.orbit.global.auth;
+
+import com.orbit.users.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticatedUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+	private final AuthService authService;
+
+	@Override
+	public boolean supportsParameter(MethodParameter parameter) {
+		return parameter.hasParameterAnnotation(AuthenticatedUser.class)
+			&& Long.class.equals(parameter.getParameterType());
+	}
+
+	@Override
+	public Object resolveArgument(
+		MethodParameter parameter,
+		ModelAndViewContainer mavContainer,
+		NativeWebRequest webRequest,
+		WebDataBinderFactory binderFactory
+	) {
+		String authorizationHeader = webRequest.getHeader("Authorization");
+		return authService.getUserFromAuthorizationHeader(authorizationHeader).getId();
+	}
+}
